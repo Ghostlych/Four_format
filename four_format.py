50e91de75eccb821e4504cb96ccc793126085c9f
Christopher Auglend <chris.auglend@gmail.com>
2018-10-24 15:14:24 +0200

    Added core four format and its test file.

---
+++ four_format.py
@@ -0,0 +1,28 @@
+class Four_format:
+    """ A tool for formatting number of N size padding with 0.
+    Designed to make binary and hex values more readable."""
+    def __init__(self, number):
+        if not isinstance(number, int):
+            raise TypeError
+        self.number = number
+        self.styles = ["x", "b"]
+
+    def __format__(self, form):
+        """ pad is the character used to pad the string.
+        length is the desired amount of characters to print.
+        style is the type of the representation; supports
+        binary and hex."""
+        if len(form) < 3:
+            raise ValueError
+        pad = form[0]
+        type = form[-1]
+        length = form.partition(type)[0]
+        length = int(length.partition(pad)[-1])
+        string = ("{:0{length}{type}}"
+                  .format(self.number, length = length, type = type))
+        step = 4
+        words = [string[i: i + step] for i in range(0, len(string), step)]
+        output = pad + type
+        for word in words:
+            output += word + " "
+        return output.rstrip()
---
+++ test_four_format.py
@@ -0,0 +1,25 @@
+import pytest
+from test_element import Test_element
+from four_format import Four_format
+
+
+class Test_four_format:
+    def set_up_string(self, string, number):
+        string = string.format(Four_format(number))
+        string = string.replace(" ", "")
+        return string
+
+    def test_binary_format(self):
+        string = "{:08b}"
+        for i in range(0xff):
+            assert "0b" + string.format(i) == self.set_up_string(string, i)
+
+    def test_hex_format(self):
+        string = "{:04x}"
+        for i in range(0xff):
+            assert "0x" + string.format(i) == self.set_up_string(string, i)
+
+    def test_correct_behavior(self):
+        assert "0b1010 1101" == "{:08b}".format(Four_format(173))
+        assert "0x0000 0000 dead beef" == "{:016x}".format(Four_format(0xdeadbeef))
+        assert "0b0000 0000 0000 1011" == "{:016b}".format(Four_format(0xb))
